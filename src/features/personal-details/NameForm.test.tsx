c0224730adfdb6d54a5e2349710e71ac901deabe
Sam Curry <sam.curry@purple.telstra.com>
2020-09-04 09:36:59 +0800

    add some more tests for data entry and form validation

--- src/features/personal-details/NameForm.test.tsx
+++ src/features/personal-details/NameForm.test.tsx
@@ -16,18 +16,58 @@ describe('NameForm', () => {
     expect(lastName).toHaveProperty('value', data.lastName);
   });
 
-  test('returns form data', async () => {
+  test('returns form data supplied via props', async () => {
     const submitHandler = jest.fn();
     const data: NameFormData = { firstName: 'John', lastName: 'Smith' };
 
     const { getByTestId } = render(
       <NameForm {...data} onSubmit={submitHandler} />
     );
-
     const submit = getByTestId('submit');
+
     userEvent.click(submit);
 
     await wait(() => expect(submitHandler).toBeCalledTimes(1));
     expect(submitHandler.mock.calls[0][0]).toEqual(data);
+  });
+
+  test('accepts and returns user input', async () => {
+    const submitHandler = jest.fn();
+    const data = { firstName: 'John', lastName: 'Smith' };
+
+    const { getByTestId } = render(<NameForm onSubmit={submitHandler} />);
+
+    const firstName = getByRole(getByTestId('firstName'), 'textbox');
+    const lastName = getByRole(getByTestId('lastName'), 'textbox');
+    const submit = getByTestId('submit');
+
+    userEvent.type(firstName, data.firstName);
+    userEvent.type(lastName, data.lastName);
+    userEvent.click(submit);
+
+    await wait(() => expect(submitHandler).toBeCalledTimes(1));
+    expect(submitHandler.mock.calls[0][0]).toEqual(data);
+  });
+
+  test('prevents partial submission', async () => {
+    const firstNameText = 'William';
+    const submitHandler = jest.fn();
+    const { getByTestId } = render(<NameForm onSubmit={submitHandler} />);
+
+    const firstName = getByRole(getByTestId('firstName'), 'textbox');
+    const lastName = getByRole(getByTestId('lastName'), 'textbox');
+    const submit = getByTestId('submit');
+
+    // enter only first name (not last name)
+    userEvent.type(firstName, firstNameText);
+
+    // first name should be filled, last name empty, submit button disabled
+    await wait(() => expect(firstName).toHaveProperty('value', firstNameText));
+    expect(lastName).toBeEmpty();
+    expect(submit).toBeDisabled();
+
+    // click of submit button should do nothing
+    userEvent.click(submit);
+    await wait(() => expect(submitHandler).toBeCalledTimes(0));
   });
 });
